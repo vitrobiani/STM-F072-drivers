f48969436cac2d55a1cc0e882eb4a0b427a8b113
vitrobiani <vitrobiani@gmail.com>
2026-04-25 14:30:39 +0300

    forgot to enable port c clock for  user button

--- src/button.c
+++ src/button.c
@@ -1,6 +1,8 @@
 #include "button.h"
 
 void enable_user_button() {
+    // Enable GPIOC clock at bit 19 of AHBENR
+    RCC_AHBENR |= (1U << 19);
     GPIOC_MODER &= ~(3U << (USER_BUTTON_PIN * 2));   // 00 for intput
 }
 
